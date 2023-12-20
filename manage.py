f8c69ec08ac59dd1d72b227e937b6e3efa4f0ab8
ketela7 <103868701+ketela7@users.noreply.github.com>
2023-12-20 10:53:55 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,10 +3,29 @@
 import warnings
 import os
 import sys
+import resource
 
 warnings.filterwarnings('ignore', category=UserWarning, module='cffi')
 
+def memory_limit():
+    """Limit max memory usage to half."""
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    # Convert KiB to bytes, and divide in two to half
+    resource.setrlimit(resource.RLIMIT_AS, (int(get_memory() * 1024 / 2), hard))
+
+def get_memory():
+    with open('/proc/meminfo', 'r') as mem:
+        free_memory = 0
+        for i in mem:
+            sline = i.split()
+            if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+                free_memory += int(sline[1])
+    print(free_memory)
+    return free_memory  # KiB
+    
 if __name__ == '__main__':
+    memory_limit()
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobsf.MobSF.settings')
 
     from django.core.management import execute_from_command_line
