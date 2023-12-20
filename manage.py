c0ae6c06fefd8bded497df394e7af109d09f2726
ketela7 <103868701+ketela7@users.noreply.github.com>
2023-12-20 17:40:33 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -13,7 +13,7 @@ def memory_limit():
     
     # Convert KiB to bytes, and divide in two to half
     #po = int(get_memory() * 1024 / 2)
-    resource.setrlimit(resource.RLIMIT_AS, (1503238553, hard))
+    resource.setrlimit(resource.RLIMIT_AS, (2147483648, hard))
 
 memory_limit()
 
