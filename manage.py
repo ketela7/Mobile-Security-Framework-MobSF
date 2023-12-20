e634c5f62087fc72968e12e523610d0260d04fb1
ketela7 <103868701+ketela7@users.noreply.github.com>
2023-12-20 19:34:40 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,16 +6,10 @@ import sys
 
 warnings.filterwarnings('ignore', category=UserWarning, module='cffi')
 
-def memory_limit():
-    import resource
-    """Limit max memory usage to half."""
-    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
-    
-    # Convert KiB to bytes, and divide in two to half
-    #po = int(get_memory() * 1024 / 2)
-    resource.setrlimit(resource.RLIMIT_AS, (2147483648, hard))
 
-memory_limit()
+from psutil import virtual_memory
+from resource import setrlimit, RLIMIT_AS
+setrlimit(RLIMIT_AS, (int(virtual_memory().available / 5), -1))
 
 if __name__ == '__main__':
     
