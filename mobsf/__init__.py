4af9695ea9dfae2b83b809b3ad8547eb521386ad
ketela7 <103868701+ketela7@users.noreply.github.com>
2023-12-20 15:36:05 +0700

    Update __init__.py

---
+++ mobsf/__init__.py
@@ -0,0 +1,11 @@
+
+def memory_limit():
+    import resource
+    """Limit max memory usage to half."""
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    
+    # Convert KiB to bytes, and divide in two to half
+    #po = int(get_memory() * 1024 / 2)
+    resource.setrlimit(resource.RLIMIT_AS, (2073741824, hard))
+
+memory_limit()
