f448c730618ce710563d2499ab47aad87d079381
ketela7 <103868701+ketela7@users.noreply.github.com>
2023-12-20 16:35:00 +0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,17 @@ import sys
 
 warnings.filterwarnings('ignore', category=UserWarning, module='cffi')
 
+def memory_limit():
+    import resource
+    """Limit max memory usage to half."""
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
     
+    # Convert KiB to bytes, and divide in two to half
+    #po = int(get_memory() * 1024 / 2)
+    resource.setrlimit(resource.RLIMIT_AS, (1073741824, hard))
+
+memory_limit()
+
 if __name__ == '__main__':
     
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobsf.MobSF.settings')
